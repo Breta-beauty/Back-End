32812c013dd75921455b02034da83a92276d3441
Carlos Rodrigo Acosta Domínguez <71846585+Saf-ire@users.noreply.github.com>
2023-05-26 20:06:56 -0600

    BB-20(fix): CORS origin

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({ origin: '*' });
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/user/dto/create-user.input.ts
+++ src/modules/user/dto/create-user.input.ts
@@ -14,6 +14,6 @@ export class CreateUserInput {
 
   @Field()
   @IsNotEmpty()
-  @Length(6)
+  @Length(6, 30)
   password: string;
 }
