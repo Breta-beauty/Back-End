985139466e800f142b85556ce745bfdbc5016546
Carlos Rodrigo Acosta Domínguez <71846585+Saf-ire@users.noreply.github.com>
2023-06-07 17:31:15 -0600

    BB-Cloud:Remove(Salon)

--- src/modules/salon/dto/update-salon.input.ts
+++
@@ -1,5 +0,0 @@
-import { CreateSalonInput } from './create-salon.input';
-import { InputType, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateSalonInput extends PartialType(CreateSalonInput) {}
