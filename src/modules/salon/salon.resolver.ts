025132bc0a36b4b23cf3e011670c25a09432c9be
Carlos Rodrigo Acosta Domínguez <71846585+Saf-ire@users.noreply.github.com>
2023-08-04 18:27:46 -0600

    BB:Cloud(Salon): Create and update address

--- src/modules/salon/salon.resolver.ts
+++ src/modules/salon/salon.resolver.ts
@@ -7,6 +7,8 @@ import { Resolver, Query, Mutation, Args, Int, ID } from '@nestjs/graphql';
 import { FindByInput } from './dto/findBy.input';
 import { CreateSalonInput } from './dto/create-salon.input';
 import { UpdateSalonInput } from './dto/update-salon.input';
+import { CreateAddressInput } from '../address/dto/create-address.input';
+import { UpdateAddressInput } from '../address/dto/update-address.input';
 
 @Resolver(() => Salon)
 export class SalonResolver {
@@ -16,8 +18,14 @@ export class SalonResolver {
   createSalon(
     @Args('user_id', { type: () => ID }) user_id: string,
     @Args('createSalonInput') createSalonInput: CreateSalonInput,
+    @Args('createAddressInput', { nullable: true })
+    createAddressInput: CreateAddressInput,
   ) {
-    return this.salonService.create(user_id, createSalonInput);
+    return this.salonService.create(
+      user_id,
+      createSalonInput,
+      createAddressInput,
+    );
   }
 
   @Query(() => [Salon], { name: 'salons' })
@@ -39,8 +47,14 @@ export class SalonResolver {
   updateSalon(
     @Args('salon_id', { type: () => ID }) salon_id: number,
     @Args('updateSalonInput') updateSalonInput: UpdateSalonInput,
+    @Args('updateAddressInput', { nullable: true })
+    updateAddressInput?: UpdateAddressInput,
   ) {
-    return this.salonService.update(salon_id, updateSalonInput);
+    return this.salonService.update(
+      salon_id,
+      updateSalonInput,
+      updateAddressInput,
+    );
   }
 
   @Mutation(() => Salon)
--- src/modules/salon/salon.service.ts
+++ src/modules/salon/salon.service.ts
@@ -8,15 +8,24 @@ import { User } from '../user/entities/user.entity';
 import { CreateSalonInput } from './dto/create-salon.input';
 import { UpdateSalonInput } from './dto/update-salon.input';
 import { FindByInput } from './dto/findBy.input';
+import { CreateAddressInput } from '../address/dto/create-address.input';
+import { Address } from '../address/entities/address.entity';
+import { AddressService } from '../address/address.service';
+import { UpdateAddressInput } from '../address/dto/update-address.input';
 
 @Injectable()
 export class SalonService {
   constructor(
     @InjectRepository(Salon) private salonRepo: Repository<Salon>,
     @InjectRepository(User) private userRepo: Repository<User>,
+    private addressService: AddressService,
   ) {}
 
-  async create(user_id: string, createSalonInput: CreateSalonInput) {
+  async create(
+    user_id: string,
+    createSalonInput: CreateSalonInput,
+    createAddressInput?: CreateAddressInput,
+  ) {
     const user = await this.userRepo.findOne({
       where: { user_id },
       relations: { profile: true },
@@ -28,6 +37,10 @@ export class SalonService {
 
     const newSalon = await this.salonRepo.create(createSalonInput);
     newSalon.owner = profile;
+
+    if (createAddressInput) {
+      this.addressService.create(newSalon.salon_id, createAddressInput);
+    }
 
     return this.salonRepo.save(newSalon);
   }
@@ -76,12 +89,23 @@ export class SalonService {
     return salon;
   }
 
-  async update(salon_id: number, updateSalonInput: UpdateSalonInput) {
-    const salon = await this.salonRepo.findOneBy({ salon_id });
+  async update(
+    salon_id: number,
+    updateSalonInput: UpdateSalonInput,
+    updateAddressInput?: UpdateAddressInput,
+  ) {
+    const salon = await this.salonRepo.findOne({
+      relations: { address: true },
+      where: { salon_id },
+    });
 
     if (!salon) throw new NotFoundException(['No se encontró el salón']);
 
     this.salonRepo.merge(salon, updateSalonInput);
+
+    if (updateAddressInput) {
+      this.addressService.update(salon.address.address_id, updateAddressInput);
+    }
 
     return this.salonRepo.save(salon);
   }
